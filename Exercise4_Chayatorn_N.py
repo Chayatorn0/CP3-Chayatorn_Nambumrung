d9f3654c19a3b91f072f652ce9eafb996f41fa95
Chayatorn0 <131854328+Chayatorn0@users.noreply.github.com>
2023-04-29 17:24:42 +0700

    Add files via upload

---
+++ Exercise4_Chayatorn_N.py
@@ -0,0 +1,9 @@
+print("--- Your Score ---")
+FE = 95
+GB = 78
+ITCS = 64
+CP = 96
+print("Foundation English : ", FE)
+print("General Business : ", GB)
+print("Introduction to Computer Systems : ", ITCS)
+print("Computer Programing : ", CP)
